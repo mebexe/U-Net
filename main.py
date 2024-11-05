4002ea98f8846601b25930b177828f3357ea6c3a
mebexe <mebexe@yahoo.com>
2024-11-05 10:39:19 +0330

    first code

---
+++ main.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as VF
+
+
+class BLOCK(nn.Module):
+    def __init__ (self):
+     pass
+
+
+
+    def forward (self, x):
+        psss
+
+
+
+
